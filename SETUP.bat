7b15584f2f55bd55fba3b252966ff20ce05f0bb5
Varacek06 <vojtavara06@email.cz>
2026-09-25 23:18:26 +0200

    CRITICAL FIX: Restore csc.exe compilation step to SETUP.bat
    All previous C# changes were invisible because SETUP.bat was only copying
    the old precompiled EXE. Now SETUP.bat compiles ReplayBridge.cs on install,
    so teVirtualMIDI fallback and all other fixes actually take effect.

--- SETUP.bat
+++ SETUP.bat
@@ -1,12 +1,48 @@
 @echo off
-echo Instaluji Blackmagic Replay Editor Bridge pro SoftLab...
+echo ========================================================
+echo   Blackmagic Replay Editor Bridge - Instalace
+echo ========================================================
+echo.
+
+echo [1/5] Ukoncuji bezici procesy...
 taskkill /F /IM ReplayBridge.exe >nul 2>&1
 taskkill /F /IM node.exe >nul 2>&1
+
+echo [2/5] Kopiruju soubory do C:\SoftLab_ReplayBridge...
+if not exist "C:\SoftLab_ReplayBridge" mkdir "C:\SoftLab_ReplayBridge"
 xcopy /E /I /Y "%~dp0bin\*" "C:\SoftLab_ReplayBridge\"
+
+echo [3/5] Kompiluju ReplayBridge.exe...
+set CSC=
+if exist "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" (
+    set CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe
+) else if exist "C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe" (
+    set CSC=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
+)
+if "%CSC%"=="" (
+    echo   !!! CHYBA: csc.exe nenalezen, pouzivam predkompilovany exe
+) else (
+    "%CSC%" /nologo /platform:x86 /target:winexe /win32icon:"C:\SoftLab_ReplayBridge\icon.ico" /r:System.Windows.Forms.dll /r:System.Drawing.dll /out:"C:\SoftLab_ReplayBridge\ReplayBridge.exe" "C:\SoftLab_ReplayBridge\ReplayBridge.cs"
+    if errorlevel 1 (
+        echo   !!! CHYBA: Kompilace selhala!
+        pause
+        exit /b 1
+    )
+    echo   - Kompilace OK
+)
+
+echo [4/5] Konfigurace systemu...
 reg import "C:\SoftLab_ReplayBridge\SoftLab_ReplayEditor.reg"
 taskkill /F /IM slgpiservers.exe >nul 2>&1
 cscript //nologo "C:\SoftLab_ReplayBridge\make_shortcut.vbs"
+
+echo [5/5] Spoustim ReplayBridge...
 start "" "C:\SoftLab_ReplayBridge\ReplayBridge.exe"
+
 echo.
-echo HOTOVO! ReplayBridge bezi potichu na pozadi (zelena ikonka u hodin).
+echo ========================================================
+echo   HOTOVO! ReplayBridge bezi (ikona u hodin).
+echo   Pokud sviti cervena, podivej se na soubor:
+echo   C:\SoftLab_ReplayBridge\midi_debug.txt
+echo ========================================================
 pause
